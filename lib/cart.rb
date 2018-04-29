84fbd55dbadbe3a96cb45406d61f3556a2341230
Tommy <sage.ts920126@gmail.com>
2018-04-29 22:05:10 +0800

    add 15% discount

--- lib/cart.rb
+++ lib/cart.rb
@@ -9,7 +9,9 @@ class Cart
   def calculate #計算客戶所買的書本總價
     total = 0
     while @ordered_items.size > 0
-      if @ordered_items.size == 3
+      if @ordered_items.size == 4
+        total = total + 4 * PRICE * 0.85
+      elsif @ordered_items.size == 3
         total = total + 3 * PRICE * 0.9
       elsif @ordered_items.size == 2 #  如果買了一組，要依原價打5%
         total = total + 2 * PRICE * 0.95
